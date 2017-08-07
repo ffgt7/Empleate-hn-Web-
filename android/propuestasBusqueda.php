7de92adc516c9c02256c0bfbb5e6335848729325
F_T <unah20121015078#gmail.com>
2017-08-07 00:49:09 -0600

    modificacion de busqueda propuestas

--- android/propuestasBusqueda.php
+++ android/propuestasBusqueda.php
@@ -1,7 +1,8 @@
 <?php
 	include("../lib/conexion.php");
 
-	$sql="select cod_propuesta,cargoP,nombreP,descripcionP from propuesta";
+	$sql="select cod_propuesta,descripcionP,imagen,cargoP from propuesta 
+	join usuarios_empre on cod_usuario=fk_userEmpre where propuesta.estado = 1";
 	$resultado= $conexion->prepare($sql);
 	$resultado->execute();
 	$n=$resultado->fetchAll();
