a83440489a9d8206b2d3f7043f1497a8a5ffe0f0
F_T <unah20121015078#gmail.com>
2017-08-08 15:31:15 -0600

    modificacion busquedaAndroid

--- android/busquedaAndroid.php
+++ android/busquedaAndroid.php
@@ -1,13 +1,13 @@
 <?php
-	include("../lib/conexion.php");
-	include "../lib/Llenado_Select.php";
-	$w=new Llenado_Select();
-	$resp=$_GET['cod'];
-	
-	$sql="select cod_propuesta as codigo, nombreP as titulo,descripcionP as descripcion,caducidadP as fecha,imagen as imagen,nomb_empre from propuesta 
-	join usuarios_empre on cod_usuario=fk_userEmpre where propuesta.estado = 1 and MATCH(cargoP) AGAINST (? IN BOOLEAN MODE ) order by caducidadP asc";
-	$resultado= $conexion->prepare($sql);
-	$resultado->execute(array($resp));
-	$n=$resultado->fetchAll();
-	$json = array("items" => $n);
-	echo json_encode($json);
+include("../lib/conexion.php");
+include "../lib/Llenado_Select.php";
+$w=new Llenado_Select();
+$resp=$_GET['cod'];
+
+$sql="select cod_propuesta as codigo, nombreP as titulo,descripcionP as descripcion,caducidadP as fecha,imagen,nomb_empre from propuesta 
+	join usuarios_empre on cod_usuario=fk_userEmpre where MATCH(cargoP) AGAINST (? IN BOOLEAN MODE ) or cargoP like '%$resp%' and estado=1 order by caducidadP asc";
+$resultado= $conexion->prepare($sql);
+$resultado->execute(array($resp));
+$n=$resultado->fetchAll();
+$json = array("items" => $n);
+echo json_encode($json);
