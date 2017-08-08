243388c422b80d2c679ded94287e0fbcef99bcbf
efrel <efli95.ealc@gmail.com>
2017-08-07 19:56:01 -0600

    jj

--- android/visitasEmprePerfil.php
+++ android/visitasEmprePerfil.php
@@ -4,7 +4,7 @@
     $w=new Llenado_Select();
 
     $cod = $_GET['cod'];
-    $sql="select nomb_user, img_perfil,num_visitas, fecha from contadorempre join usuarios_empleo on 
+    $sql="select nomb_user as nombre, img_perfil as imagen,num_visitas as numVisitas, fecha from contadorempre join usuarios_empleo on 
     contadorempre.cod_visitante = usuarios_empleo.cod_empleo where contadorempre.cod_perfil = (select cod_usuario 
     from usuarios_empre where nomb_usuario='$cod')";
     $array=$w->llenarSelect($sql);
