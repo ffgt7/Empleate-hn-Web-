f255dda0d84d5c3a98822d1f8aad02ed3b930c2a
ffgt <unah20121050078@gmail.com>
2017-08-08 01:00:25 -0600

    Ver Mensajes Usuario Listo3

--- android/verMsjUser.php
+++ android/verMsjUser.php
@@ -9,7 +9,7 @@
 	include "../lib/Llenado_Select.php";
 	$w=new Llenado_Select();
     $cod=$_GET["cod"];
-	$sql5="select CodMensajee,asunto,texto,de,fk_Usuario,imagen,fecha,nomb_empre,nomb_usuario,cod_usuario from usuarios_empre join  mensajese on 
+	$sql5="select CodMensajee,nombres,asunto,texto,de,fk_Usuario,imagen,fecha,nomb_empre,nomb_usuario,cod_usuario from usuarios_empre join  mensajese on 
 	cod_usuario=de join usuarios_empleo on cod_empleo=fk_Usuario where CodMensajee=$cod";
 	$array=$w->llenarSelect($sql5);
 	$json = array("items" => $array);
--- android/verMsjUserEnvi.php
+++ android/verMsjUserEnvi.php
@@ -9,8 +9,8 @@ require "../lib/conexion.php";
 include "../lib/Llenado_Select.php";
 $w=new Llenado_Select();
 $cod=$_GET["cod"];
-$sql5="select CodMensaje,asunto,texto,de,fk_UsuarioEm,fecha,nomb_empre,imagen from mensaje join usuarios_empre on
- cod_usuario=fk_UsuarioEm where CodMensaje=$cod";
+$sql5="select CodMensaje, nombres,asunto,texto,de,fk_UsuarioEm,fecha,nomb_empre,imagen from mensaje join usuarios_empre on
+ cod_usuario=fk_UsuarioEm join usuarios_empleo on cod_empleo=de where CodMensaje=$cod";
 $array=$w->llenarSelect($sql5);
 $json = array("items" => $array);
 $items=json_encode($json);
