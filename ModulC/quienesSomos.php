ff603d0fb574b202810e6b53b9db986acda886bc
F_T <unah20121015078#gmail.com>
2017-08-20 15:11:25 -0600

    actualizar contra empre

--- ModulC/quienesSomos.php
+++ ModulC/quienesSomos.php
@@ -10,46 +10,50 @@
 <?php 
 	require "nav.php";
 ?>
-	<div id="home" >
+<div id="home" >
 	<div class="container">
-		<div class="row">
-			<div class="col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2 ">
-				<div id="carousel-slider" data-ride="carousel" class="carousel slide  animate-in" data-anim-type="fade-in-up">
-					<div class="carousel-inner">
-						<div class="item active">
-							<h3 style="font-family:'fantasy'; font-size:50px;">¡Quiero Chamba!</h3>
-							<p style="font-family:'fantasy'; color:white; font-size:20px;">Es un sitio web orientado al ambito profesional,<br>aquí podras encontrar el empleo que mejor se adapte<br>a las necesidades que buscas.<br>&nbsp; </p>
-						</div>
-						<div class="item">
-							<h3 style="font-family:'fantasy'; font-size:50px;">Consultas o Comentarios</h3>
-							<p style="font-family:'fantasy'; color:white; font-size:20px;">Email :  webQuieroChamba@gmail.com<br><br>Celular :  +(504) 9617-9007<br>&nbsp; </p>
-						</div>
-						<div class="item">
-							<h3 style="font-family:'fantasy'; font-size:50px;">Diseño</h3>
-							<p style="font-family:'fantasy'; color:white; font-size:20px;">Christopher Wilfredo Gonzalez Bermudez<br>Efrel Armando López Cáceres<br>Friedland Frederik Gonzalez Trejo<br>Kristy Alejandra Rivera Ardon</p>
+        
+<div class="col-sm-12 col-md-12 col-lg-12">
+            <div class="row">
+				
+					<div class="col-sm-12 col-md-4 col-lg-4 ">
+						<img src="../img/logo2.png" class="img img-responsive"/>
+					</div>
+					<div class="col-sm-12 col-md-8 col-lg-8 ">
+						<div id="carousel-slider" data-ride="carousel" class="carousel slide  animate-in" data-anim-type="fade-in-up">
+							<div class="carousel-inner">
+								<div class="item active">
+									<h3 style="font-family:'fantasy'; font-size:50px;">Empleate-HN</h3>
+									<p style="font-family:'fantasy'; color:white; font-size:20px;">Es un sitio web orientado al ambito profesional,<br>aquí podras encontrar el empleo que mejor se adapte<br>a las necesidades que buscas.<br>&nbsp; </p>
+								</div>
+								<div class="item">
+									<h3 style="font-family:'fantasy'; font-size:50px;">Consultas o Comentarios</h3>
+									<p style="font-family:'fantasy'; color:white; font-size:20px;">Email :  webQuieroChamba@gmail.com<br><br>Celular :  +(504) 9617-9007<br>&nbsp; </p>
+								</div>
+								<div class="item">
+									<h3 style="font-family:'fantasy'; font-size:50px;">Diseño</h3>
+									<p style="font-family:'fantasy'; color:white; font-size:20px;">Christopher Wilfredo Gonzalez Bermudez<br>Efrel Armando López Cáceres<br>Friedland Frederik Gonzalez Trejo<br>Kristy Alejandra Rivera Ardon</p>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-		<div class="row animate-in" data-anim-type="fade-in-up">
-		<div class="col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3 col-lg-8 col-lg-offset-2 scroll-me">
+            </div>
+			
+			
+            <div class="row animate-in" data-anim-type="fade-in-up">
+                <div class="col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3 col-lg-8 col-lg-offset-2 scroll-me">
+                    <p style="font-family:'fantasy'; font-size:20px;">¡Siguenos en nuestras redes sociales!</p>
+                    <div class="social">
+                        <a href="https://www.facebook.com/profile.php?id=100016987111019" target="_blank" class="btn button-custom btn-custom-one" ><i class="fa fa-facebook "></i></a>
+                        <a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-google-plus "></i></a>
+                    </div>
+                    <p class="w3-label">© 2017 | by Elementary Redux  </p>
+                </div>
+            </div>
 
-
-		<p style="font-family:'fantasy'; font-size:20px;">¡Siguenos en nuestras redes sociales!</p>
-		<div class="social">
-		<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-facebook "></i></a>
-		<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-twitter"></i></a>
-		<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-google-plus "></i></a>
-		<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-linkedin "></i></a>
-		<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-pinterest "></i></a>
-		<a href="#" class="btn button-custom btn-custom-one" ><i class="fa fa-github "></i></a>
-		</div>
-		<p class="w3-label">© 2017 | by Grupo2  </p>
-		</div>
-		</div>
+        
 	</div>
-
 </div>
 <!--HOME SECTION END-->
 </body>
