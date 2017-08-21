a4fbb80d3d3df917b95c334cae91f4dc7364c732
F_T <unah20121015078#gmail.com>
2017-08-20 20:56:34 -0600

    actualizar contra empre

--- android/curriHabilidades.php
+++ android/curriHabilidades.php
@@ -3,7 +3,7 @@
 	include "../lib/Llenado_Select.php";
 	$w=new Llenado_Select();
     $cod=$_GET["cod"];
-  $sql5="select habilidad, nombAplica, nivel from curriHabilidades join habilidades on cod_habilidad=fk_habilidad join aplicaciones on cod_aplicacion=fk_aplicacion
+  $sql5="select cod_curriHabi, habilidad, nombAplica, nivel from curriHabilidades join habilidades on cod_habilidad=fk_habilidad join aplicaciones on cod_aplicacion=fk_aplicacion
 	join nivelidiom on cod_nivel=fk_nivelHabi where fk_userEmpleo=(select cod_empleo from usuarios_empleo where
    nomb_user='$cod')";
 	$array=$w->llenarSelect($sql5);
