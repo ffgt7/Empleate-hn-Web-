edb623188f4a626ef936a196bee371299dcb2a8b
F_T <unah20121015078#gmail.com>
2017-08-22 15:06:54 -0600

    agregado nomb_user a curriReciEmpre

--- android/curriReciEmpre.php
+++ android/curriReciEmpre.php
@@ -3,7 +3,7 @@
 	
 	$codPro=$_GET['cod']; 
 	
-	$sql1="select DISTINCT requi_nocumple,cod_envio,cod_empleo,nombres,img_perfil,concat_ws(',', tituloObtenidoS, tituloObtenidoSecu) as titulo, 
+	$sql1="select DISTINCT requi_nocumple,cod_envio,nomb_user,nombres,img_perfil,concat_ws(',', tituloObtenidoS, tituloObtenidoSecu) as titulo, 
 	YEAR(CURDATE())-YEAR(Fech_Naci) + IF(DATE_FORMAT(CURDATE(),'%m-%d') > DATE_FORMAT(Fech_Naci,'%m-%d'), 0, -1) AS `EDAD_ACTUAL` from usuarios_empleo 
 	left join educacion on cod_empleo=fk_userEdu left join curri_expelabo on fk_userExpeLbo=cod_empleo 
 	left join curri_idioma on fk_userIdioma=cod_empleo left join enviocurri on fk_userDesem=cod_empleo
