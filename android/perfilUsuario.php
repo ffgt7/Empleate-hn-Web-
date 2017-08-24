132bda1420a4d915421aed14a925cb1e6734a807
F_T <unah20121015078#gmail.com>
2017-08-24 01:37:00 -0600

    perfil usuario actualizado

--- android/perfilUsuario.php
+++ android/perfilUsuario.php
@@ -5,7 +5,7 @@
 	include "../lib/Llenado_Select.php";
 	$w=new Llenado_Select();
     $cod=$_GET["cod"];
-    $sql5="select apellidos, cod_empleo,email_user,direccion,Fech_regisUser,Fech_Naci,img_perfil,nombres,identidadC,sexo,tel_fijo,tel_movil,depart,nacionalidad,muni
+    $sql5="select descrip_userEmpleo,nomb_user,apellidos,cod_empleo,email_user,direccion,Fech_regisUser,Fech_Naci,nombres,img_perfil,identidadC,sexo,tel_fijo,tel_movil,depart,nacionalidad,muni
 	from usuarios_empleo join departamentos on cod_depart=fk_departamento join nacionalidades on cod_nacion=fk_nacionalida join municipios on cod_muni=fk_municipio
 	where nomb_user='$cod'";
 	$array=$w->llenarSelect($sql5);
